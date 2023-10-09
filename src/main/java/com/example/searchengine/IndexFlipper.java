1d93d8e62d6c1ca11b70935abe2e28a735aa56ba
nicolaskeller <nicolas.keller@student.unisg.ch>
2023-10-09 17:17:51 +0200

    Implemented the indexflipper class and generated output file, passed the test, task 2 done

--- src/main/java/com/example/searchengine/IndexFlipper.java
+++ src/main/java/com/example/searchengine/IndexFlipper.java
@@ -7,24 +7,49 @@ import org.springframework.stereotype.Component;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.*;
+
 @Component
 public class IndexFlipper {
 
-    public void flipIndex(String indexFileName, String flippedIndexFileName){
+    public void flipIndex(String indexFileName, String flippedIndexFileName) {
         try {
             CSVReader csvReader = new CSVReader(new FileReader(indexFileName));
             List<String[]> csvLines = csvReader.readAll();
+
+            // Map to store words and corresponding URLs
+            Map<String, Set<String>> wordToURLMap = new HashMap<>();
+
+            for (String[] csvLine : csvLines) {
+                String url = csvLine[0];
+                for (int i = 1; i < csvLine.length; i++) {
+                    wordToURLMap.computeIfAbsent(csvLine[i], k -> new HashSet<>()).add(url);
+                }
+            }
+
             Set<String[]> lines = new HashSet<>();
-            //TODO: define lines to contain the lines that should be printed to index_flipped.csv
-            CSVWriter writer = new CSVWriter(new FileWriter(flippedIndexFileName),',', CSVWriter.NO_QUOTE_CHARACTER,' ',"\r\n");
+
+            for (Map.Entry<String, Set<String>> entry : wordToURLMap.entrySet()) {
+                String word = entry.getKey();
+                Set<String> urls = entry.getValue();
+
+                String[] line = new String[urls.size() + 1];
+                line[0] = word;
+                int idx = 1;
+                for (String url : urls) {
+                    line[idx++] = url;
+                }
+
+                lines.add(line);
+            }
+
+            CSVWriter writer = new CSVWriter(new FileWriter(flippedIndexFileName), ',', CSVWriter.NO_QUOTE_CHARACTER, ' ', "\n");
             for (String[] line : lines) {
                 writer.writeNext(line);
             }
+            writer.close();
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
