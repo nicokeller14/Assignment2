d4ce9931c7ea3a9bc69e7f4112567fadd195eb00
nicolaskeller <nicolas.keller@student.unisg.ch>
2023-10-12 16:06:24 +0200

    added endpoints for seachengine

--- src/main/java/com/example/searchengine/SearchEngine.java
+++ src/main/java/com/example/searchengine/SearchEngine.java
@@ -4,12 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
@@ -47,5 +54,36 @@ public class SearchEngine {
 			indexFlipper.flipIndex(indexFileName, flippedIndexFileName);
 		}
 	}
+
+	@GetMapping("/search")
+	public ResponseEntity<List<String>> search(@RequestParam("q") String query) {
+		// Perform the search using the 'searcher' component
+		List<String> urls = searcher.search(query, flippedIndexFileName);
+		return ResponseEntity.ok(urls);
+	}
+
+
+	@GetMapping("/lucky")
+	public ResponseEntity<String> lucky(@RequestParam("q") String query) {
+		// Perform the search, but only retrieve the first result
+		List<String> urls = searcher.search(query, flippedIndexFileName);
+		return (urls.isEmpty()) ? ResponseEntity.notFound().build() : ResponseEntity.ok(urls.get(0));
+	}
+
+
+	@GetMapping("/")
+	public ResponseEntity<String> mainPage() {
+		// Assuming you have an HTML file under resources/static/index.html
+		Resource resource = (Resource) new ClassPathResource("static/index.html");
+		try {
+			String content = new String(Files.readAllBytes(Path.of("static/index.html")));
+			return ResponseEntity.ok(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error loading page");
+		}
+	}
+
+
 
 }
